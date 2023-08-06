696758754225978662d61e50f1b9e2c6335fc7df
Kaan Koç <kock@mef.edu.tr>
2023-08-06 16:07:57 +0300

    fix: closes #1

--- post-merge.js
+++ post-merge.js
@@ -2,5 +2,5 @@
 var shell = require('shelljs');
 
 shell.exec(
-  'post-merge": "git push origin dev --tags && git checkout main && git push origin main --tags'
+  'git push origin dev --tags && git checkout main && git push origin main --tags'
 );
