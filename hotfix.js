057199ee1f414284ef66a786f54a5ea080a0ecd1
Kaan Koç <kock@mef.edu.tr>
2023-12-14 16:43:41 +0300

    docs: version bump

--- hotfix.js
+++ hotfix.js
@@ -25,39 +25,39 @@ console.log('🚀 ~ file: hotfix.js:26 ~ isGhAvailable:', isGhAvailable);
 
 if (isLolcatAvailable) {
   // Use the provided version to create a hotfix branch
-  exec(`echo Creating hotfix ${version} `);
-  exec(`git flow hotfix start ${version} `);
-  exec('echo Hotfix branch created ');
+  shell.exec(`echo Creating hotfix ${version} | lolcat`);
+  shell.exec(`git flow hotfix start ${version} | lolcat`);
+  shell.exec('echo Hotfix branch created | lolcat');
   // Rest of your commands with lolcat
-  exec('echo Hotfix works ');
-  exec('npm version patch ');
-  exec('echo made version patch ');
-  exec('git add . && git commit -m "docs: version bump"');
-  exec('echo committed ');
-  exec('git flow hotfix publish ');
-  exec('echo published the branch 🎉 ');
+  shell.exec('echo Hotfix works | lolcat');
+  shell.exec('npm version patch | lolcat');
+  shell.exec('echo made version patch | lolcat');
+  shell.exec('git add . && git commit -m "docs: version bump"');
+  shell.exec('echo committed | lolcat');
+  shell.exec('git flow hotfix publish | lolcat');
+  shell.exec('echo published the branch 🎉 | lolcat');
   if (isGhAvailable) {
-    exec(
+    shell.exec(
       `gh pr create --title "Hotfix ${version}" --body "Hotfix ${version}" --base main`
     );
   }
-  exec('echo created the PR 🎉 ');
+  shell.exec('echo created the PR 🎉 | lolcat');
 } else {
   // Use the provided version to create a hotfix branch
-  exec(`echo Creating hotfix ${version}`);
-  exec(`git flow hotfix start ${version}`);
-  exec('echo Hotfix branch created');
+  shell.exec(`echo Creating hotfix ${version}`);
+  shell.exec(`git flow hotfix start ${version}`);
+  shell.exec('echo Hotfix branch created');
 
   // Rest of your commands
-  exec('echo Hotfix works');
-  exec('npm version patch');
-  exec('echo made version patch');
-  exec('git add . && git commit -m "docs: version bump"');
-  exec('echo committed');
-  exec('git flow hotfix publish');
-  exec('echo published the branch 🎉');
+  shell.exec('echo Hotfix works');
+  shell.exec('npm version patch');
+  shell.exec('echo made version patch');
+  shell.exec('git add . && git commit -m "docs: version bump"');
+  shell.exec('echo committed');
+  shell.exec('git flow hotfix publish');
+  shell.exec('echo published the branch 🎉');
   if (isGhAvailable) {
-    exec(
+    shell.exec(
       `gh pr create --title "Hotfix ${version}" --body "Hotfix ${version}" --base main`
     );
   }
